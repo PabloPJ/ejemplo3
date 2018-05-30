804fc5bc7e92a478da168065ad6580cee2ce5437
TheoRevFdz <hrevillafernandez79@gmail.com>
2018-05-30 17:40:39 -0500

    Correccion del desatrede Pablo

--- src/com/example/ejercicio3/MainActivity.java
+++ src/com/example/ejercicio3/MainActivity.java
@@ -6,10 +6,17 @@ import android.view.Menu;
 
 public class MainActivity extends Activity {
 
+    private EditText etLetra;
+    private Button btnCalcular;
+    private TextView tvResult;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViews();
+        listeners();
     }
 
 
@@ -18,6 +25,36 @@ public class MainActivity extends Activity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
+    }
+
+    private void findViews() {
+        etLetra = findViewById(R.id.etLetra);
+        btnCalcular = findViewById(R.id.btnCalcular);
+        tvResult = findViewById(R.id.tvResult);
+    }
+
+    private void listeners() {
+        btnCalcular.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tvResult.setText(String.valueOf(calcular()));
+            }
+        });
+    }
+
+    private double calcular() {
+        double sum = 0;
+        for (int i = 0; i < etLetra.getText().toString().length(); i++) {
+            char l = etLetra.getText().toString().charAt(i);
+            if (l == 'A') {
+                sum += (5000 * 0.02);
+            } else if (l == 'B') {
+                sum += (4000 * 0.02);
+            } else {
+                sum += (3000 * 0.02);
+            }
+        }
+        return sum;
     }
     
 }
